a632361280d4719dc3789054d3655681792f866d
spoletaev <info.elekit@yandex.ru>
2022-09-05 20:48:57 +0300

    1.1. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println("Name first thread is " + Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println("Name second thread is " + Thread.currentThread().getName())
+        );
+        System.out.println("State first thread is " + first.getState());
+        System.out.println("State second thread is " + second.getState());
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            System.out.println("State first thread is " + first.getState());
+            System.out.println("State second thread is " + second.getState());
+        }
+        System.out.println("State first thread after loop " + first.getState());
+        System.out.println("State second thread after loop " + second.getState());
+        System.out.println("The work is done");
+    }
+}
