4f9abaafb8a1df90fac17c5f171fe493dfb5c93a
spoletaev <info.elekit@yandex.ru>
2022-09-05 23:48:36 +0300

    3. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,28 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    private final char[] process = new char[]{'\\', '|', '/'};
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(10000); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        try {
+            int count = 0;
+            while (!Thread.currentThread().isInterrupted()) {
+                Thread.sleep(500);
+                System.out.print("\r load: " + process[count++]);
+                if (count > 2) {
+                    count = 0;
+                }
+            }
+        } catch (InterruptedException ignored) {
+
+        }
+    }
+}
