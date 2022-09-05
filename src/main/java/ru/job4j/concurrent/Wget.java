8a6a3ffa5034d46bd4624c723201161b9cecbea2
spoletaev <info.elekit@yandex.ru>
2022-09-05 21:17:53 +0300

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,20 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    for (int i = 0; i <= 100; i++) {
+                        try {
+                            Thread.sleep(1000);
+                            System.out.print("\r loading : " + i + "%");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        thread.start();
+    }
+}
