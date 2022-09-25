10dfdf6c5c2ba61db43eabc551997c49f50893fe
spoletaev <info.elekit@yandex.ru>
2022-09-25 15:46:19 +0300

    0. Управление нитью через wait. [#6858]

---
+++ src/main/java/ru/job4j/wait/CountBarrier.java
@@ -0,0 +1,30 @@
+package ru.job4j.wait;
+
+public class CountBarrier {
+    private final Object monitor = this;
+    private final int total;
+    private volatile int count;
+
+    public CountBarrier(int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (monitor) {
+            count++;
+            monitor.notifyAll();
+        }
+    }
+
+    public void await() {
+        synchronized (monitor) {
+            while (count < total) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
